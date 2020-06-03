0d25909edaf7845b86709b0c375fd5112d99df24
David Padilla <dpadman2@gmail.com>
2020-06-02 21:18:32 -0500

    names

---
+++ names.rb
@@ -0,0 +1,23 @@
+# You have 5 names in the 'names' array
+# The name is 'Michael Choi'
+# The name is 'John Doe'
+# The name is 'Jane Doe'
+# The name is 'James Smith'
+# The name is 'Jennifer Smith'
+
+
+a = {first_name: "Michael", last_name: "Choi"}
+b = {first_name: "John", last_name: "Doe"}
+c = {first_name: "Jane", last_name: "Doe"}
+d = {first_name: "James", last_name: "Smith"}
+e = {first_name: "Jennifer", last_name: "Smith"}
+names = [a, b, c, d, e]
+
+def print_names array
+    puts "You have #{array.length} names in the 'names' array"
+    array.each do |i|
+        puts "The name is, '#{i[:first_name]} #{i[:last_name]}'"
+    end
+end
+
+print_names (names)
